dfaf6f4c0cef7e9dda4843871752d5fdadf1e360
cubicwork <cubicwork@qq.com>
2019-02-05 22:30:00 +0800

    finish basic code

--- scripts/render.js
+++ scripts/render.js
@@ -5,6 +5,8 @@ var Render = (function (helper) {
   _self._state = null
   _self._playground = null
 
+  var canvasID = '__canvas_playground'
+
   function initialPlayGround (newState) {
     // assume an cell with 5px width and 5px height
     if (_self._mode === 0 || _self._mode === 1) {
@@ -19,7 +21,30 @@ var Render = (function (helper) {
         }
       }
     } else if (_self._mode === 2 || _self._mode === 3) {
-
+      // prepare canvas object
+      var canvas = document.getElementById(canvasID)
+      if (!canvas) {
+        canvas = document.createElement('canvas')
+        canvas.setAttribute('id', canvasID)
+        canvas.setAttribute('width', newState[1].length * 10)
+        canvas.setAttribute('height', newState[0].length * 10)
+        canvas.addEventListener('click', handleClick)
+        _self._playground.append(canvas)
+      }
+      // draw reacangles
+      var drawable = canvas.getContext("2d")
+      drawable.clearRect(0, 0, drawable.width, drawable.height)
+      drawable.lineWidth = '1'
+      drawable.strokeStyle="#ddd"
+      for (var vIndex = 0; vIndex < newState.length; vIndex ++) {
+        for (var hIndex = 0; hIndex < newState[vIndex].length; hIndex ++) {
+          // draw path
+          drawable.strokeRect(10 * hIndex, 10 * vIndex, 10, 10)
+          // fill
+          drawable.fillStyle = newState[vIndex][hIndex] ? '#FF0' : '#333'
+          drawable.fillRect(10 * hIndex, 10 * vIndex, 9, 9)
+        }
+      }
     }
     _self._state = helper.matrixDeepCopy(newState)
   }
@@ -47,6 +72,52 @@ var Render = (function (helper) {
           }
         }
       }
+    } else if (_self._mode === 2) {
+      // dom with direct refresh
+      var canvas = document.getElementById(canvasID)
+      if (!canvas) {
+        canvas = document.createElement('canvas')
+        canvas.setAttribute('id', canvasID)
+        canvas.setAttribute('width', newState[1].length * 10)
+        canvas.setAttribute('height', newState[0].length * 10)
+        canvas.addEventListener('click', handleClick)
+        _self._playground.append(canvas)
+      }
+      var drawable = canvas.getContext("2d")
+      drawable.clearRect(0, 0, drawable.width, drawable.height)
+      drawable.lineWidth = '1'
+      drawable.strokeStyle="#ddd"
+      for (var vIndex = 0; vIndex < newState.length; vIndex ++) {
+        for (var hIndex = 0; hIndex < newState[vIndex].length; hIndex ++) {
+          // draw path
+          drawable.strokeRect(10 * hIndex, 10 * vIndex, 10, 10)
+          // fill
+          drawable.fillStyle = newState[vIndex][hIndex] ? '#FF0' : '#333'
+          drawable.fillRect(10 * hIndex, 10 * vIndex, 9, 9)
+        }
+      }
+    } else if (_self._mode === 3) {
+      var canvas = document.getElementById(canvasID)
+      if (!canvas) {
+        canvas = document.createElement('canvas')
+        canvas.setAttribute('id', canvasID)
+        canvas.setAttribute('width', newState[1].length * 10)
+        canvas.setAttribute('height', newState[0].length * 10)
+        _self._playground.append(canvas)
+      }
+      var drawable = canvas.getContext("2d")
+      drawable.clearRect(0, 0, drawable.width, drawable.height)
+      drawable.lineWidth = '1'
+      drawable.strokeStyle="#ddd"
+      for (var vIndex = 0; vIndex < newState.length; vIndex ++) {
+        for (var hIndex = 0; hIndex < newState[vIndex].length; hIndex ++) {
+          if (_self._state && _self._state[vIndex] && newState[vIndex][hIndex] !== _self._state[vIndex][hIndex]) {
+            // fill
+            drawable.fillStyle = newState[vIndex][hIndex] ? '#FF0' : '#333'
+            drawable.fillRect(10 * hIndex, 10 * vIndex, 9, 9)
+          }
+        }
+      }
     }
     _self._state = helper.matrixDeepCopy(newState)
   }
@@ -58,7 +129,20 @@ var Render = (function (helper) {
         _self.onClick(parseInt(targetData[1]), parseInt(targetData[2]))
       }
     } else if (_self._mode === 2 || _self._mode === 3) {
-
+      var targetX = ev.clientX
+      var targetY = ev.clientY
+      var offsetX = 0
+      var offsetY = 0
+      var canvas = document.getElementById(canvasID)
+      while (canvas && canvas.offsetLeft !== undefined) {
+        offsetX += parseFloat(canvas.offsetLeft)
+        offsetY += parseFloat(canvas.offsetTop)
+        canvas = canvas.parentNode
+      }
+      console.log(targetX, targetY, offsetX, offsetY)
+      var h = Math.floor((targetX - offsetX) / 10)
+      var v = Math.floor((targetY - offsetY + 30) / 10)
+      _self.onClick(v, h)
     }
   }
 
